89ad3ec695c08f5cd3033d84e1720d57e01b96ef
donkeyrat <darklordbananas@gmail.com>
2022-10-02 14:33:06 -0700

    Create AKRandomChance.cs

---
+++ AKRandomChance.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace AnimalKingdom
+{
+    public class AKRandomChance : MonoBehaviour
+    {
+        public void Start()
+        {
+            if (onStart) { Randomize(); }
+        }
+
+        public void Randomize()
+        {
+            if (Random.value > randomChance)
+            {
+                randomEvent.Invoke();
+            }
+        }
+
+        public UnityEvent randomEvent;
+
+        public float randomChance;
+
+        public bool onStart;
+    }
+}
