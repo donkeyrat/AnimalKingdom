92892cdf488085ea685423a8811ed4c7a68d7669
donkeyrat <darklordbananas@gmail.com>
2022-10-02 14:35:35 -0700

    Bump version

--- AKLauncher.cs
+++ AKLauncher.cs
@@ -2,7 +2,7 @@
 
 namespace AnimalKingdom
 {
-	[BepInPlugin("teamgrad.animalkingdom", "Animal Kingdom", "0.2.1")]
+	[BepInPlugin("teamgrad.animalkingdom", "Animal Kingdom", "0.3.0")]
 	public class AKLauncher : BaseUnityPlugin
 	{
 		public AKLauncher()
